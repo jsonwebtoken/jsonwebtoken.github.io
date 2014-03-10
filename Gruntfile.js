475ca1f6e46bf922106c5f9ea1fcb6f8179078a4
Alberto Pose <albertopose@gmail.com>
2014-03-10 20:52:26 -0300

    Simplifying Gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,24 +10,8 @@ module.exports = function (grunt) {
         options: {
           livereload: true
         },
-        files: ['Gruntfile.js', 'js/**/*.js', 'less/**/*.less', 'index.html'],
+        files: ['Gruntfile.js', 'js/**/*.js', 'less/**/*.less', 'html/index.html'],
         tasks: ['build']
-      }
-    },
-    // copy: {
-    //   dist: {
-    //     files: [{
-    //       expand: true,
-    //       dot: true,
-    //       dest: 'tmp',
-    //       src: [ 'css/**', 'js/**', 'vendor/**', 'index.html' ]
-    //     }],
-    //   }
-    // },
-    uglify: {
-      options: {
-        compress: false,
-        beautify: true
       }
     },
     connect: {
@@ -48,40 +32,6 @@ module.exports = function (grunt) {
         files: {
           'css/app.css': 'less/app.less'
         }
-      }
-    },
-    s3: {
-      options: {
-        key:    process.env.S3_KEY,
-        secret: process.env.S3_SECRET,
-        bucket: process.env.S3_BUCKET,
-        access: 'public-read',
-        headers: {
-          'Cache-Control':  'public, max-age=300'
-        }
-      },
-      publish: {
-        upload: [{
-          src:    'dist/*',
-          dest:   '/',
-          options: { gzip: true }
-        }]
-      }
-    },
-    invalidate_cloudfront: {
-      options: {
-        key:            process.env.S3_KEY,
-        secret:         process.env.S3_SECRET,
-        distribution:   process.env.CDN_DISTRIBUTION
-      },
-      production: {
-        files: [{
-          expand:   true,
-          cwd:      './dist/',
-          src:      ['**/*'],
-          filter:   'isFile',
-          dest:     '/'
-        }]
       }
     },
     useminPrepare: {
