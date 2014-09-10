66a3d5f152157fe9e09ff073fcc886ef5affebdd
anaibol <juananibal.micheli@gmail.com>
2014-09-10 01:14:05 -0300

    Started with some layout changes

--- js/app.js
+++ js/app.js
@@ -69,10 +69,10 @@
 
     if (!value) { return; }
 
-    var parts = value.split('.');
+    // var parts = value.split('.');
 
-    var secretElement = document.getElementsByName('secret')[0];
-    var signatureElement = getFirstElementByClassName('js-signature');
+    // var secretElement = document.getElementsByName('secret')[0];
+    // var signatureElement = getFirstElementByClassName('js-signature');
 
     if (!signatureElement) {
       return;
@@ -191,7 +191,7 @@
 
   loadFromStorage(function (jwt) {
     tokenEditor.setValue(
-      jwt || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEyMzQ1Njc4OTAsIm5hbWUiOiJKb2huIERvZSIsImFkbWluIjp0cnVlfQ.eoaDVGTClRdfxUZXiPs3f8FmJDkDE_VCQFXqKxpLsts'
+      jwt || 'fZJNT%2BMwEIbvSPwHy%2Fd8tMvHympSdUGISuwS0cCBm%2BtMUwfbk%2FU4zfLvSVMq2Euv45n3fd7xzOb%2FrGE78KTRZXwSp5yBU1hpV2f8ubyLfvJ5fn42I2lNKxZd2Lon%2BNsBBTZMOhLjQ8Y77wRK0iSctEAiKLFa%2FH4Q0zgVrceACg1ny9uMy7rCdaM2%2Bs0BWrtppK2UAdeoVjW2ruq1bevGImcvR6zpHmtJ1MHSUZAuDKU0vY7Si2h6VU5%2BiMuJuLx65az4dPql3SHBKaz1oYnEfVkWUfG4KkeBna7A%2Fxm6M14j1gZihZazBRH4MODcoKPOgl%2BB32kFz08PGd%2BG0JJIkr7v46%2BhRCaEpod17DCRivYZCkmkd4N28B3wfNyrGKP5bws9DS6PKDz%2FMpsl36Tyz%2F%2Fax1jeFmi0emcLY7C%2F8SDD0Z7dobcynHbbV3QVbcZW0TlqQemNhoqzJD%2B4%2Fn8Yw7l8AA%3D%3D'
     );
 
   });
---
+++ js/saml.js
@@ -0,0 +1,114 @@
+
+// XXX Hack to prevent hextorstr function used by JWS send a string instead of
+// a Word Array. On this way, no string decoding needs to take place and Crypto
+// takes care of everything.
+// Note that it should not affect the other algorithms as hextorstr is exclusively
+// used on Hmac family (that invokes CryptoJS library).
+window.hextorstr = function (c) {
+  return window.CryptoJS.enc.Hex.parse(c);
+};
+
+
+//this is used to parse base64
+function url_base64_decode(str) {
+  var output = str.replace('-', '+').replace('_', '/');
+  switch (output.length % 4) {
+    case 0:
+      break;
+    case 2:
+      output += '==';
+      break;
+    case 3:
+      output += '=';
+      break;
+    default:
+      throw 'Illegal base64url string!';
+  }
+  return window.atob(output); //polifyll https://github.com/davidchambers/Base64.js
+}
+
+window.decode = function (base64json) {
+  var json = null, error = null;
+  try {
+    json = url_base64_decode(base64json);
+    json = JSON.stringify(JSON.parse(json), undefined, 2);
+  } catch (e) {
+    error = e;
+  }
+  return {result: json, error: error};
+};
+
+window.sign = function (header, payload, secret, isSecretBase64Encoded) {
+  var value = '', error = null, headerAsJSON, payloadAsJSON;
+
+  try {
+    headerAsJSON = JSON.stringify(JSON.parse(header));
+  } catch (e) {
+    error = {result: null, error: {cause: e, who: ['header']}};
+  }
+  try {
+    payloadAsJSON = JSON.stringify(JSON.parse(payload));
+  } catch (e) {
+    if (error) {
+      error.error.who.push('payload');
+    } else {
+      error = {result: null, error: {cause: e, who: ['payload']}};
+    }
+  }
+
+  if (error) {
+    return error;
+  }
+
+  if (isSecretBase64Encoded) {
+    try {
+      secret = window.b64utob64(secret);
+      secret = window.CryptoJS.enc.Base64.parse(secret).toString();
+    } catch (e) {
+      return {result: '', error: e};
+    }
+  } else {
+    secret = window.CryptoJS.enc.Latin1.parse(secret).toString();
+  }
+
+  try {
+    value = KJUR.jws.JWS.sign(null, headerAsJSON, payloadAsJSON, secret);
+  } catch (e) {
+    error = e;
+  }
+
+  return {result: value, error: error};
+};
+
+window.isValidBase64String = function (s) {
+  try {
+    s = window.b64utob64(s);
+    window.CryptoJS.enc.Base64.parse(s).toString();
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
+window.verify = function (value, secret, isSecretBase64Encoded) {
+  var result = '', error = null;
+
+  if (isSecretBase64Encoded) {
+    try {
+      secret = window.b64utob64(secret);
+      secret = window.CryptoJS.enc.Base64.parse(secret).toString();
+    } catch (e) {
+      return {result: '', error: e};
+    }
+  } else {
+    secret = window.CryptoJS.enc.Latin1.parse(secret).toString();
+  }
+
+  try {
+    result = KJUR.jws.JWS.verify(value, secret);
+  } catch (e) {
+    error = e;
+  }
+
+  return {result: result, error: error};
+};
