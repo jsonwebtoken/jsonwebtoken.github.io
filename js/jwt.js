4faea629196a24aa76d0a573f4d94356b5d6b93f
Carl A Dunham <cdunham@gmail.com>
2016-01-19 12:19:54 -0800

    [UNTESTED] Added support for other Algos

--- js/jwt.js
+++ js/jwt.js
@@ -65,7 +65,7 @@ window.sign = function (algorithm, header, payload, key, isSecretBase64Encoded) 
     return error;
   }
 
-  if(algorithm === 'HS256'){
+  if(algorithm.match(/HS[0-9]{3,}/)){
     if (isSecretBase64Encoded) {
       try {
         key = window.b64utob64(key);
@@ -101,7 +101,7 @@ window.verify = function (algorithm, value, key, isSecretBase64Encoded) {
 
   var result = '', error = null;
 
-  if (algorithm === 'HS256'){
+  if (algorithm.match(/HS[0-9]{3,}/)){
     if (isSecretBase64Encoded) {
       try {
         key = window.b64utob64(key);
