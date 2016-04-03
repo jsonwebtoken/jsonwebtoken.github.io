0e5192a1af93cfd002699892fb383f776a5415a2
Matias Woloski <matiasw@gmail.com>
2016-04-02 21:14:59 -0300

    middleware

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ var http = require('http');
 var enforce = require('express-sslify');
  
 var app = express();
-app.use('/', express.static(__dirname));
 
 // use HTTPS(true) in case you are behind a load balancer (e.g. Heroku) 
 if (process.env.NODE_ENV === 'production') {
 	console.log('redirecting to ssl');
 	app.use(enforce.HTTPS({ trustProtoHeader: true }))
 }
+
+app.use('/', express.static(__dirname));
  
 http.createServer(app).listen(process.env.PORT || 3000, function() {
 	console.log('started');
