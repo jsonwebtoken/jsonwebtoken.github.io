7339a571eef7523fe4658d44c06f1b94e2d81583
Sebastián Peyrott <sebastian.peyrott@auth0.com>
2016-06-17 19:34:02 -0300

    Inline install for Chrome's extension.

--- js/app.js
+++ js/app.js
@@ -699,6 +699,37 @@ $('.stars').each(function(idx, element){
     }
 });
 
+$('#extension-button').on('click', function() {
+    function isChrome() {
+        // please note,
+        // that IE11 now returns undefined again for window.chrome
+        // and new Opera 30 outputs true for window.chrome
+        // and new IE Edge outputs to true now for window.chrome
+        // and if not iOS Chrome check
+        // so use the below updated condition
+        var isChromium = window.chrome,
+            winNav = window.navigator,
+            vendorName = winNav.vendor,
+            isOpera = winNav.userAgent.indexOf("OPR") > -1,
+            isIEedge = winNav.userAgent.indexOf("Edge") > -1,
+            isIOSChrome = winNav.userAgent.match("CriOS");
+
+        if(isIOSChrome){
+           return false;
+        } else if(isChromium !== null && isChromium !== undefined && vendorName === "Google Inc." && isOpera == false && isIEedge == false) {
+           return true;
+        } else {
+           return false;
+        }
+    }
+
+    if(isChrome()) {
+        chrome.webstore.install();
+    } else {
+        window.open('https://chrome.google.com/webstore/detail/jwt-debugger/ppmmlchacdbknfphdeafcbmklcghghmd');
+    }
+});
+
 //CANVAS
 // $(function(){
 //   var canvas = document.querySelector('canvas'),
