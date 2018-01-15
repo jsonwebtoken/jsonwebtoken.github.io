c343c6ee7fb3c2f545b082c06c21f3fbb93a4585
Sebastián E. Peyrott <sebastian.peyrott@auth0.com>
2018-01-15 19:07:19 -0300

    Remove unnecessary code for the extension.

--- src/extension/index.js
+++ src/extension/index.js
@@ -3,26 +3,9 @@ import {
   setTokenEditorValue,
   useDefaultToken
 } from '../editor';
-import { getParameterByName } from '../utils.js';
 import { publicKeyTextArea } from './dom-elements.js';
 
 /* For initialization, look at the end of this file */
-
-function parseLocationQuery() {
-  const publicKey = getParameterByName('publicKey');
-  const value = getParameterByName('value');
-  const token = getParameterByName('token');
-
-  if(publicKey) {
-    publicKeyTextArea.value = publicKey;
-  }
-  if(value) {
-    setTokenEditorValue(value);
-  }
-  if(token) {
-    setTokenEditorValue(token);
-  }
-}
 
 function loadToken() {
   const lastToken = localStorage.getItem('lastToken');
@@ -41,4 +24,3 @@ function loadToken() {
 // Initialization
 setupTokenEditor();
 loadToken();
-parseLocationQuery();
