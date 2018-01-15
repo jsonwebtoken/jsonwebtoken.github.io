a1c48a1b9c428a35a244557f46b2617da69b988f
Sebastián E. Peyrott <sebastian.peyrott@auth0.com>
2018-01-15 19:04:18 -0300

    Reencode the token when the Base64 secret checkbox is toggled.

--- src/editor/index.js
+++ src/editor/index.js
@@ -313,6 +313,8 @@ function setupEvents() {
 
   // HMAC secret, when changed the encoded token must be updated.
   eventManager.addDomEvent(secretInput, 'input', encodeToken);
+  // Base64 checkbox, when changes the encoded token must be updated.
+  eventManager.addDomEvent(secretBase64Checkbox, 'input', encodeToken);
   // Private key, when changed the encoded token must be updated.
   eventManager.addDomEvent(privateKeyTextArea, 'input', encodeToken);
   // Public key, when changed the encoded token must NOT be updated
