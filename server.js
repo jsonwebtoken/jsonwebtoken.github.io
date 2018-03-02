b365d0dc8fa12a759e59781752665e24b471e4e0
Sebastián E. Peyrott <sebastian.peyrott@auth0.com>
2018-03-02 20:13:37 -0300

    Heroku deployment requirements.

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const enforce = require('express-sslify');
+
+const app = express();
+
+if(process.env.NODE_ENV === 'production') {
+  console.log('Redirecting to TLS endpoint.');  
+	app.use(enforce.HTTPS({
+    // Required for proper use under a reverse proxy (Heroku, etc.).
+    trustProtoHeader: true
+  }));
+}
+
+app.use('/', express.static('dist/website'));
+
+app.listen(process.env.PORT || 3000, function() {
+	console.log('Started.');
+});
