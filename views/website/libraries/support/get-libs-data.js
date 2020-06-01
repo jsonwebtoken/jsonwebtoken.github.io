295d2bd472b8446c036c708a59095f04ce007b0f
nov <nov@matake.jp>
2020-06-01 09:03:43 +0900

    ES256K support

--- views/website/libraries/support/get-libs-data.js
+++ views/website/libraries/support/get-libs-data.js
@@ -50,7 +50,7 @@ function getLibs() {
       orderedKeys = ['hs256', 'hs384', 'hs512',
                     'rs256', 'rs384', 'rs512',
                     'es256', 'es384', 'es512'
-                    'ps256', 'ps384', 'ps512', 'eddsa'];
+                    'ps256', 'ps384', 'ps512', 'eddsa', 'es256k'];
 
       for(let i = 0; i < orderedKeys.length; ++i) {
         r.support[orderedKeys[i]] =
