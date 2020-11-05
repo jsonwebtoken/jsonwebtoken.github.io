6f55cbcbc7a46e7465acba798a47f50055b961c1
Sambego <sambellen@gmail.com>
2020-11-05 10:04:35 +0100

    Update extension permissions

--- src/extension/content-scripts/check-install.js
+++
@@ -1,4 +0,0 @@
-const e = document.getElementById('extension-button');
-if(e) {
-    e.classList.add('is-installed');
-}
--- webpack.extension-dev.js
+++ webpack.extension-dev.js
@@ -1,16 +1,15 @@
-const merge = require('webpack-merge');
+const merge = require("webpack-merge");
 
-const common = require('./webpack.common.js');
+const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-  mode: 'development',
-  entry: {
-    index: './src/extension/index.js',
-    webstorage: './src/extension/injected/webstorage.js',
-    'check-install': './src/extension/content-scripts/check-install.js'
-  },
-  output: {
-    filename: '[name].js',
-    path: __dirname + '/dist/extension/js'
-  }
+    mode: "development",
+    entry: {
+        index: "./src/extension/index.js",
+        webstorage: "./src/extension/injected/webstorage.js",
+    },
+    output: {
+        filename: "[name].js",
+        path: __dirname + "/dist/extension/js",
+    },
 });
