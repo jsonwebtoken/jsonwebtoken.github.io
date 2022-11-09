a16410b309783e06a4ba5bc9d7f79fd1d68162b3
Filip Skokan <panva.ip@gmail.com>
2022-11-09 16:34:09 +0100

    sort library language cards by language name

--- views/website/libraries/support/get-languages.js
+++ views/website/libraries/support/get-languages.js
@@ -23,5 +23,19 @@ module.exports = function getLanguages() {
         result.push(data);
     });
 
+    result.sort((a, b) => {
+        const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+        const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+        if (nameA < nameB) {
+            return -1;
+        }
+        if (nameA > nameB) {
+            return 1;
+        }
+
+        // names must be equal
+        return 0;
+    })
+
     return result;
 };
