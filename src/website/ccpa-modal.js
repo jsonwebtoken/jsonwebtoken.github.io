0363eb55c8b09f6584549c836566f5b074e9f27b
florcaputti <flor.caputti@auth0.com>
2022-12-19 11:22:09 -0300

    open modal on hash url

--- src/website/ccpa-modal.js
+++ src/website/ccpa-modal.js
@@ -1,5 +1,8 @@
 export function CCPAModal() {
   document.addEventListener("DOMContentLoaded", function () {
+    if(window.location.hash.substring(1) === 'your-privacy-choices') {
+      document.querySelector("#ccpa-modal").style.display = "block";
+    }
     document.querySelector("#open-ccpa").addEventListener("click", () => {
       document.querySelector("#ccpa-modal").style.display = "block";
     });
