0c31c8e81dee1f2c49b0e07679f6a869792b3591
florcaputti <flor.caputti@auth0.com>
2022-12-21 10:02:18 -0300

    PR review

--- src/website/ccpa-modal.js
+++ src/website/ccpa-modal.js
@@ -1,6 +1,6 @@
 export function CCPAModal() {
   document.addEventListener("DOMContentLoaded", function () {
-    if(window.location.hash.substring(1) === 'your-privacy-choices') {
+    if(window.location.hash.includes('your-privacy-choices')) {
       document.querySelector("#ccpa-modal").style.display = "block";
     }
     document.querySelector("#open-ccpa").addEventListener("click", () => {
