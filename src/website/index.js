940333831ab5b6d6bc903007ce79d70a0342b3a8
florcaputti <flor.caputti@auth0.com>
2023-04-14 16:54:37 -0300

    Add top banner

--- src/website/index.js
+++ src/website/index.js
@@ -11,7 +11,7 @@ import {
     shareJwtTextElement,
 } from "./dom-elements.js";
 import { CCPAModal } from "./ccpa-modal.js";
-import { TopBar } from "./topbar.js";
+import { TopBanner } from "./top-banner.js";
 
 import queryString from "querystring";
 
@@ -56,4 +56,4 @@ setupHighlighting();
 setupJwtCounter();
 setupShareJwtButton(shareJwtButton, shareJwtTextElement);
 CCPAModal();
-TopBar();
+TopBanner();
