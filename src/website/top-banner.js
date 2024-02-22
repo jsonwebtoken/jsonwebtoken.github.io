e6526743329f58dde2099d259584a9a727dd56bc
Byron Motoche <116190812+byron-okta@users.noreply.github.com>
2024-02-22 14:05:31 -0500

    fix: mobile nav banner not visible due top-banner-bg

--- src/website/top-banner.js
+++ src/website/top-banner.js
@@ -3,6 +3,7 @@ export function TopBanner() {
     document
       .querySelector(".close-top-banner")
       .addEventListener("click", () => {
+        document.querySelector(".top-banner-bg").classList.add("closed");
         document.querySelector(".top-banner").classList.add("closed");
         document.querySelector(".top-banner-spacer").classList.add("hide");
         document.querySelector(".navbar").classList.remove("top-banner-open");
