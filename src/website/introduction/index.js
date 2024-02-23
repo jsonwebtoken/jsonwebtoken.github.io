8e46978aa9224ede30d1923931865307a7b0d874
Byron Motoche <116190812+byron-okta@users.noreply.github.com>
2024-02-23 09:42:22 -0500

    fix tippy top banner not closing on introduction and libraries pages

--- src/website/introduction/index.js
+++ src/website/introduction/index.js
@@ -1,6 +1,8 @@
 import { setupHighlighting } from "../highlighting.js";
 import { setupJwtCounter } from "../counter.js";
+import { TopBanner } from "../top-banner.js";
 
 // Initialization
 setupHighlighting();
 setupJwtCounter();
+TopBanner();
--- src/website/libraries/index.js
+++ src/website/libraries/index.js
@@ -1,8 +1,10 @@
 import { setupLibraries } from "./libraries.js";
 import { setupHighlighting } from "../highlighting.js";
 import { setupJwtCounter } from "../counter.js";
+import { TopBanner } from "../top-banner.js";
 
 // Initialization
 setupLibraries();
 setupHighlighting();
 setupJwtCounter();
+TopBanner();
