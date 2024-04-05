fd7b0d06376dc6cdeb4a3990f00797ca85aa6a86
Byron Motoche <116190812+byron-okta@users.noreply.github.com>
2024-04-05 14:31:02 -0500

    feature: promote new webinar

--- src/website/libraries/index.js
+++ src/website/libraries/index.js
@@ -2,6 +2,7 @@ import { CCPAModal } from "../ccpa-modal.js";
 import { setupJwtCounter } from "../counter.js";
 import { setupHighlighting } from "../highlighting.js";
 import { setupNavbar } from "../navbar.js";
+import { TopBanner } from "../top-banner.js";
 import { setupLibraries } from "./libraries.js";
 
 // Initialization
@@ -10,3 +11,4 @@ setupLibraries();
 setupHighlighting();
 setupJwtCounter();
 CCPAModal();
+TopBanner();
