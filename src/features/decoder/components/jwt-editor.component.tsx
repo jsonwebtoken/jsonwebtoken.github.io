ce857b19d2fba0e7a3a21bf44ccfeb427ba9c2d0
Javier Tinoco <213990346+javiert-okta@users.noreply.github.com>
2025-07-21 12:25:20 -0500

    add autofocus prop to jwt editor component

--- src/features/decoder/components/jwt-editor.component.tsx
+++ src/features/decoder/components/jwt-editor.component.tsx
@@ -1,21 +1,23 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { EditorComponent } from "@/features/common/components/code-editor/editor.component";
 
 interface JwtEditorComponentProps {
   token: string;
   handleJwtChange: (value: string) => void;
+  autoFocus: boolean
 }
 
 export const JwtEditorComponent: React.FC<JwtEditorComponentProps> = ({
   token,
+  autoFocus,
   handleJwtChange,
 }) => {
   return (
     <EditorComponent
       aria-label="JWT editor input"
       value={token}
-      autoFocus
-      focusOnWindowFocus
+      autoFocus={autoFocus}
+      focusOnWindowFocus={autoFocus}
       onValueChange={(code) => handleJwtChange(code)}
       highlight={(code) => {
         if (!code) {
